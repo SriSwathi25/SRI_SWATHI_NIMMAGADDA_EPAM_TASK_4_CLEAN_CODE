5bd8a5f30cb1842d50fe3b209adde6a708fb6c94
Sri Swathi Nimmagadda <45977029+SriSwathi25@users.noreply.github.com>
2020-02-20 22:24:07 +0530

    Add files via upload

--- Console.java
+++ Console.java
@@ -1,27 +1,11 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 public class Console{
-	public void calculate()throws IOException
-	{
-		Scanner s=new Scanner(System.in);
-		System.out.println("1. Simple Interest 2. Compound Interest\n");
-		int n=s.nextInt();		
-		switch(n) {
-		case 1 : SimpleInterest value1=new SimpleInterest();
-					value1.simpleInterest();
-					break;
-		case 2 : CompoundInterest value2=new CompoundInterest();
-					value2.compoundInterest();
-					break;
-					
-		default : System.out.println("Enter valid number !!");
-		}
-
-	}
-	public static void main(String[] args)throws IOException
-	{	
-		Console obj=new Console();
-		obj.calculate();
-	}
-
+public static void main(String args[])throws IOException{
+    ConstructionCost house=new ConstructionCost();
+	house.takeInput();
+    long cost=house.computeCost(house.materialChoice, house.Automated);
+	System.out.println("Cost of building your house is :"+cost+" INR");
+    
+}
 }
